1eb431b96b5c2b6aacbe006243e23089e1eff062
araaa262 <dodiksurati283@gmail.com>
2026-09-15 10:09:44 +0700

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -79,9 +79,9 @@ const PAUSE_DURATION = 2000;
 const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
-  { icon: Github, link: "https://github.com/EkiZR", label: "GitHub Profile" },
-  { icon: Linkedin, link: "https://www.linkedin.com/in/ekizr/", label: "LinkedIn Profile" },
-  { icon: Instagram, link: "https://www.instagram.com/ekizr_/?hl=id", label: "Instagram Profile" }
+  { icon: Github, link: "https://github.com/araaa262", label: "GitHub Profile" },
+  { icon: Linkedin, link: "https://www.linkedin.com/in/ancklo/", label: "LinkedIn Profile" },
+  { icon: Instagram, link: "https://www.instagram.com/rassss1/?hl=id", label: "Instagram Profile" }
 ];
 
 const Home = () => {
